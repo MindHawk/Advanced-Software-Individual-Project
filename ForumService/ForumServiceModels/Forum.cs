299f573f633a9bd092d7f381603aa7f9012f4ace
Tim Gooren <384376@student.fontys.nl>
2022-11-09 14:33:40 +0100

    Add model validation

--- ForumService/ForumServiceModels/Forum.cs
+++ ForumService/ForumServiceModels/Forum.cs
@@ -1,8 +1,13 @@
-﻿namespace ForumServiceModels;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForumServiceModels;
 
 public class Forum
 {
+    [Key]
     public int Id { get; set; }
+    [Required, StringLength(60, MinimumLength = 3)]
     public string Name { get; set; }
+    [Required, StringLength(300)]
     public string Description { get; set; }
 }
