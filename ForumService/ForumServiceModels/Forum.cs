d1bbc3e7092b5249d259ac86c48b4d0585d49ebb
Tim Gooren <384376@student.fontys.nl>
2022-11-11 13:19:52 +0100

    Auto migrate db

--- ForumService/ForumServiceModels/Forum.cs
+++ ForumService/ForumServiceModels/Forum.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ForumServiceModels;
 
 public class Forum
 {
     [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
     [Required, StringLength(60, MinimumLength = 3)]
     public string Name { get; set; }
