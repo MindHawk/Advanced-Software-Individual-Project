10cae93bbe15b0a0cc16f645e11118f6b3f9d462
Tim Gooren <384376@student.fontys.nl>
2022-11-14 09:00:57 +0100

    Update migrations

---
+++ ForumService/ForumServiceAPI/Migrations/20221111132050_second.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ForumServiceAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Forums",
+                table: "Forums");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Forums");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "adminId",
+                table: "Forums",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Forums",
+                table: "Forums",
+                column: "Name");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Forums",
+                table: "Forums");
+
+            migrationBuilder.DropColumn(
+                name: "adminId",
+                table: "Forums");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Forums",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Forums",
+                table: "Forums",
+                column: "Id");
+        }
+    }
+}
