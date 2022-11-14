2296daa138fc1a6917de2614ab8347356bad3cbc
Tim Gooren <384376@student.fontys.nl>
2022-11-14 09:00:42 +0100

    Don't allow special characters in forum name

--- ForumService/ForumServiceModels/Forum.cs
+++ ForumService/ForumServiceModels/Forum.cs
@@ -8,6 +8,7 @@ public class Forum
     // Forum names may never be shared with other forums; they are a unique identifier.
     [Key, Required, StringLength(60, MinimumLength = 3)]
     public string Name { get; set; }
+    [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Special characters and spaces are not allowed.")]
     [Required, StringLength(300)]
     public string Description { get; set; }
     public Guid adminId { get; set; }
