91ad69ee88d2d5bb85cd5dc832b500a83e211f44
Tim Gooren <384376@student.fontys.nl>
2023-01-04 10:53:07 +0100

    Add title to post

--- PostService/PostServiceModels/Post.cs
+++ PostService/PostServiceModels/Post.cs
@@ -7,6 +7,8 @@ public class Post
 {
     [Key]
     public int Id { get; set; }
+    [Required, StringLength(60, MinimumLength = 3)]
+    public string Title { get; set; }
     [Required, StringLength(2000, MinimumLength = 1)]
     public string Content { get; set; }
     [Required]
