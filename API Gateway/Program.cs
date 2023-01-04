deb2069517d14bedd5d418ed0e405564ce816bb7
Tim Gooren <384376@student.fontys.nl>
2023-01-04 10:53:17 +0100

    Fix cors origin

--- API Gateway/Program.cs
+++ API Gateway/Program.cs
@@ -20,7 +20,7 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(b =>
     {
-        b.WithOrigins("http://localhost:5173/").AllowAnyHeader().AllowAnyMethod();
+        b.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod();
     });
 });
 
